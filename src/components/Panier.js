bcf5de9195386485509f394b499c2e2256f32ab5
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2022-02-28 18:14:16 +0100

    feat: get current url in web page

--- src/components/Panier.js
+++ src/components/Panier.js
@@ -1,35 +1,24 @@
-import React from 'react'
-import './Panier.css'; 
-import account from '../icons/account.png'
-import accountHover from '../icons/account_hover.png'
-const Panier = ()  => {
+/* global chrome */
+
+import React, { useEffect, useState } from 'react'
+import './Panier.css';
+
+const Panier = () => {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = tabs[0].url;
+      setUrl(url);
+    });
+  }, []);
+  
   return (
     <div>
-      <header>
-        <nav class="top-bar">
-          <a href="#" class="top-bar-logo">Mi cesta</a>
-          <ul class="top-bar-list">
-            <li class="top-bar-username"><a href="#">User Name</a></li>
-            <li class="top-bar-username-icon"><a href="#">
-              <img src={account} alt="account1" class="account" />
-              <img src={accountHover} alt="account2" class="account-hover" />
-            </a></li>
-          </ul>
-        </nav>
-      </header>
-
-
-
-      <footer>
-        <div class="bottom-bar">
-          <ul class="bottom-bar-total">
-            <li class="bottom-bar-text">Total Amount</li>
-            <li class="bottom-bar-sum">$364</li>
-          </ul>
-          <a href="#" class="checkout-button">Check out</a>
-        </div>
-      </footer>
+      {url}
     </div>
   )
 }
- export default Panier
+export default Panier

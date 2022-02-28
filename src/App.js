e7af1a9ef5c721862b6b49517e7f031f148d727d
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2022-02-28 18:13:13 +0100

    style: header panier and footer separated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+/* global chrome */
+
 import logo from './logo.svg';
 import './App.css';
 import Panier from './components/Panier';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Panier />
-    </div>
+      <Footer />
+      </div>
   );
 }
 

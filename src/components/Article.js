126d43f4d6e81e6e3a3502b90f6379b04bd4a9c6
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2022-03-01 14:59:12 +0100

    feat: article interface

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Article.css'
+const Article = () => {
+  return (
+    <li className='article'>
+      
+      <div className='description'>
+        description
+      </div>
+      <div className='price'>
+
+      </div>
+      ListArticle
+    </li>
+  )
+}
+export default Article

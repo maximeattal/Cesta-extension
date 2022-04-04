89f6b29ae74a9325473065a27fb83ca5be337a57
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2022-04-05 00:37:31 +0200

    feat: remove article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import './Article.css'
 import asosArticle from '../icons/asosarticle.jpeg'
-import { ButtonGroup, Button } from '@mui/material'
+import { ButtonGroup, Button, IconButton } from "@mui/material";
 import { fontFamily, height } from '@mui/system'
 
-const Article = ({ article }) => {
+const Article = ({ marchandId, articleId, article, handleRemoveArticle }) => {
   const [quantity, setQuantity] = useState(1)
   const [size, setSize] = useState('M')
   return (
     <li className="article">
+      <Button
+        variant="contained"
+        disableElevation
+        onClick={() => handleRemoveArticle(marchandId, articleId)}
+        sx={{
+          position: "absolute",
+          padding: "0px",
+          minWidth: "0px",
+          width: "17px",
+          height: "17px",
+          top: "0px",
+          right: "0px",
+          backgroundColor: "#f2f2f2",
+          "&:hover": {
+            backgroundColor: "#DC143C",
+          },
+        }}
+      >
+        x
+      </Button>
       <div className="article-image">
         <a href={article.site} target="_blank">
           <img
